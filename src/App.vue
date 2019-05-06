007a2a28c064feb7a64b2cfc58b4c9a868fdf75f
Bestian Tang 唐宗浩 <besitan@gmail.com>
2019-05-06 09:49:00 +0800

    add a news

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         i.home.icon
       .right.menu
         .item
-          iframe(src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fxikxik.bestian.tw&layout=button_count&size=small&appId=485195848253155&width=64&height=20" width="64" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media")
+          iframe(src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fxikxik.bestian.tw&layout=button_count&size=small&appId=485195848253155&width=70&height=20" width="70" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media")
     router-view
 </template>
 
