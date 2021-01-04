edeaa133e89317dc9167216cef9b6b4ff0a87062
Bestian Tang <bestian@gmail.com>
2021-01-04 11:42:22 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  runtimeCompiler: true,
+  css: { sourceMap: true },
+  productionSourceMap: true,
+  pwa: {
+    workboxOptions: {
+      exclude: [/\.gitkeep/]
+    }
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/'
     : '/'
